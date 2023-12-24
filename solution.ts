9d32cdfa45d4665d624fa13d2a74045859f75af9
Kooreis <kooreis.official@gmail.com>
2023-12-24 00:14:41 -0600

    This update introduces the ladderLength function, which takes in a beginWord, an endWord, and a wordList. It first converts the wordList into a Set for faster lookup. If the endWord is not in the wordSet, it returns 0. It then creates a queue and adds a new Node with the beginWord and a step count of 1 to the queue.

--- solution.ts
+++ solution.ts
@@ -1,8 +1,6 @@
-class Node {
-    word: string;
-    steps: number;
-    constructor(word: string, steps: number) {
-        this.word = word;
-        this.steps = steps;
-    }
+function ladderLength(beginWord: string, endWord: string, wordList: string[]): number {
+    let wordSet = new Set(wordList);
+    if (!wordSet.has(endWord)) return 0;
+    let queue: Node[] = [];
+    queue.push(new Node(beginWord, 1));
 }
